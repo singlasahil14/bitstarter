5f645dd8131d04d6c8490aa57ed3c44991815992
Sahil Singla <singlasahil14@gmail.com>
2014-03-28 13:29:57 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
-var fs = require('fs'); 
+var fs = require('fs');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
-var data = fs.readFileSync('/Users/ubuntu@ip-172-31-11-6/bitstarter/index.html');
+var data = fs.readFileSync('bitstarter/index.html');
 app.get('/', function(request, response) {
   response.send('Hello world 2');
 });
